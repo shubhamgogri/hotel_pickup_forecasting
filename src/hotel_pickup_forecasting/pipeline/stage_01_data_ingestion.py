9f35e9ef1e19429979cb2df6b5d2515b762f7776
Shubham <gogrishubham85@gmail.com>
2024-06-12 22:22:15 +0100

    Data Ingestion

---
+++ src/hotel_pickup_forecasting/pipeline/stage_01_data_ingestion.py
@@ -0,0 +1,31 @@
+from hotel_pickup_forecasting.components.data_ingestion import DataIngestion
+from hotel_pickup_forecasting.config.configuration import ConfigurationManager
+from hotel_pickup_forecasting import logger
+
+
+
+STAGE_NAME = "Data Ingestion stage"
+
+class DataIngestionTrainingPipeline:
+    def __init__(self):
+        pass
+
+    def main(self):
+        config = ConfigurationManager()
+        data_ingestion_config = config.get_data_ingestion_config()
+        data_ingestion = DataIngestion(config=data_ingestion_config)
+        data_ingestion.download_file()
+        data_ingestion.extract_zip_file()
+
+
+    
+if __name__ == '__main__':
+    try:
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = DataIngestionTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+    except Exception as e:
+        logger.exception(e)
+        raise e
+
